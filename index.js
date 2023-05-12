26eab9b6ce6ec3bfc464ecce015f9463c5574c9e
Daniel Ruiz <danielruimar@gmail.com>
2023-05-12 12:08:50 +0200

    fix problems

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const main = async () => {
   const server = express(); // Definimos el server. Lo gestionará express.
   server.use(express.json()); // Sepa interpretar los JSON
   server.use(express.urlencoded({ extended: false })); //  Sepa interpretar bien los parametros de las rutas.
-  server.use(cors({ origin: "*" })); // Utilice la libreria cors para gestionar la seguridad de acceso a la API
+  server.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001", "https://node-s4-exercise.vercel.app"] })); // Utilice la libreria cors para gestionar la seguridad de acceso a la API
 
   // Definimos el routerHome que será el encargado de manejar las peticiones a nuestras rutas en la raíz.
   const routerHome = express.Router();
